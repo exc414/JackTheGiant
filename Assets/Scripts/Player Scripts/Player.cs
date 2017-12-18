1227a0973169870f8fd6fc6e2abb4560c620c344
exc414base <androidbluephoenix@gmail.com>
2017-12-17 22:39:18 -0500

    First commit.

---
+++ Assets/Scripts/Player Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour 
+{
+	public float speed = 8f;
+	public float maxVelocity = 4f;
+
+	private Rigidbody2D playerBody;
+	private Animator animator;
+
+	//Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	//Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+} //Player
