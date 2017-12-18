af25a716055d70b0bd7510fb8875fc15f810ea14
exc414base <androidbluephoenix@gmail.com>
2017-12-17 23:36:06 -0500

    Added player movement to the game.

--- Assets/Scripts/Player Scripts/Player.cs
+++ Assets/Scripts/Player Scripts/Player.cs
@@ -10,16 +10,57 @@ public class Player : MonoBehaviour
 	private Rigidbody2D playerBody;
 	private Animator animator;
 
-	//Use this for initialization
-	void Start () 
+	//Called first, Start second and then Update
+	void Awake()
+	{
+		//Gets the component from the game object
+		playerBody = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
+	}
+
+	void Start() 
 	{
 		
 	}
 	
-	//Update is called once per frame
-	void Update () 
+	//This function gets called every single frame. 60 FPS = 60 calls
+	void Update() { }
+
+	//Gets updated every couple of frames. Could 3 to 4. Best place to put code related
+	//to physics.
+	void FixedUpdate()
 	{
-		
+		playerMoveKeyboard();
+	}
+
+	void playerMoveKeyboard()
+	{
+		float forceX = 0f;
+		float velocity = Mathf.Abs(playerBody.velocity.x);
+
+		//Using raw it gives -1 (Left) 0 (Nothing) 1 (Right)
+		//Gives the direction of the player.
+		float horizontalAxis = Input.GetAxisRaw("Horizontal");
+
+		if(horizontalAxis > 0)
+		{
+			//Moving to the right (forward). Apply positive speed
+			if(velocity < maxVelocity)
+			{
+				forceX = speed;
+			}
+		}
+		else if(horizontalAxis < 0)
+		{
+			if (velocity < maxVelocity)
+			{
+				//Moving to the left (backwards). Apply negative speed
+				forceX = -speed;
+			}
+		}
+
+		//Apply the set force onto the player depending on forward/backward speed.
+		playerBody.AddForce(new Vector2(forceX, 0));
 	}
 
 } //Player
